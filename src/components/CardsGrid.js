58a5c7a2f3c37e47fc3bd55ce2914fb00cef7712
renhubgit <renik2001@gmail.com>
2022-06-11 13:53:42 +0200

    added pokemon render to CardsGrid

--- src/components/CardsGrid.js
+++ src/components/CardsGrid.js
@@ -1,5 +1,31 @@
 import React from "react"
+import styled from "styled-components"
 
-export default function CardsGrid({ fetchPokemons }) {
-  return <div>CardsGrid</div>
+export default function CardsGrid({ pokemons }) {
+  const pokemonCards = pokemons.map((pokemon) => {
+    const { id, name, image } = pokemon
+    return (
+      <div key={id} className="pokemon-card">
+        <img src={image} alt="" />
+        {name}
+      </div>
+    )
+  })
+  return <GridStyled>{pokemonCards}</GridStyled>
 }
+
+const GridStyled = styled.div`
+  display: grid;
+  gap: 5rem;
+  grid-template-columns: repeat(4, 1fr);
+  @media (max-width: 900px) {
+    grid-template-columns: repeat(3, 1fr);
+  }
+  @media (max-width: 700px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+
+  .pokemon-card {
+    width: 15rem;
+  }
+`
